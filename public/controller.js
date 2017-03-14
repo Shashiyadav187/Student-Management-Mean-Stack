8b6d4757dc3486ca840465bb082a7992821bf78c
Nisarg Shah <test@youremail.com>
2017-03-13 21:01:38 -0400

    First commit

---
+++ public/controller.js
@@ -0,0 +1,215 @@
+var app = angular.module('myapp', ['ngRoute']);
+
+app.config(['$routeProvider', function ($routeProvider) {
+	 $routeProvider
+
+	.when('/home', {
+		templateUrl: 'home.html',
+		controller: 'NameCtrl'
+	})
+	
+	.when('/register', {
+		templateUrl: 'register.html',
+		controller: 'RegisterCtrl'
+	})
+
+	.when('/login', {
+		templateUrl: 'login.html',
+		controller: 'LoginCtrl'
+	})
+
+	.when('/', {
+		templateUrl: 'login.html',
+		controller: 'LoginCtrl'
+	})
+
+	.when('/monitor', {
+		templateUrl: 'monitor.html',
+		controller: 'NameCtrl'
+	})
+
+
+	.otherwise({ 
+		redirectTo: '/login' 
+	});
+}]);
+
+
+app.controller('RegisterCtrl', ['$scope','$http', '$location', function($scope, $http, $location) {
+        
+       $scope.register = function() {
+
+       		console.log($scope.user);
+
+       		$http.post("/register", $scope.user).success(function(response) {
+       			console.log("Response received");
+       			$location.path("/login");
+       		})
+       };
+       
+      }]);
+    
+/*app.controller('LoginCtrl', ['$scope','$http', '$window','$location', function ($scope,$http,$window,$location) {
+
+		$scope.check = function() {
+		console.log($scope.user);
+
+		$http.get("/register").success(function(response) {
+		console.log("Response received");
+
+		$scope.register = response;
+		console.log(response[0].username);
+		var mydata = JSON.stringify($scope.register);
+		console.log(mydata[0].username);
+
+		for (var i in $scope.register) {
+			if($scope.register[i].username == $scope.user.username && $scope.register[i].password == $scope.user.password){
+
+				$location.path("/home");
+			}
+			else{
+				$location.path("/");
+			}
+		}
+		})	
+
+	}
+}])*/
+
+
+app.controller('NameCtrl', ['$scope','$http','$rootScope','$location', function ($scope, $http,$rootScope,$location) {
+
+	//$scope.names = ["Javascript", "Php", "Java"];
+
+	console.log("Hello World from Controller");
+
+	$http.get('/class').success(function(response) {
+		//$scope.names = JSON.stringify(response.class);
+		console.log(response);
+		$scope.names = response;
+	})
+
+	 $http.get('/isloggedin')
+        .success(function (resp) {
+            if(!resp){
+                $location.path('/login');
+            }else {
+                $scope.username = resp.username;
+                $scope.password = resp.password;
+                $rootScope.logUser = resp;
+            }
+        });
+	
+	$scope.getAverage = function(){
+    var avg = 0;
+    for(var i = 0; i < $scope.stu_info.length; i++){
+        var key = $scope.stu_info[i];
+        total = (key.mark1 + key.mark2 + key.mark3);
+    }
+    avg=total/3;
+    return avg;
+
+
+}
+
+	$scope.populate_students = function() {
+		console.log("Populate students");
+
+		var selectedclass = $scope.stu.selectedClass;
+		console.log(selectedclass);
+		$http.get("/studentlist/"+selectedclass).success(function(response) {
+			console.log(response);
+			$scope.students = response;		
+		})
+	}
+
+	$scope.display_info = function() {
+		console.log("Display information");
+
+		var selstu = $scope.stu.selectedStudent.name;
+		console.log(selstu);
+
+
+		$http.get("/getstudent/"+selstu).success(function(response) {
+			console.log(response);
+			$scope.stu_info = response;		
+		})
+/*
+		$http.post("/studentlist",{"name":selstu}).success(function(response) {
+			console.log(response);
+			$scope.stu_info = response;		
+		})*/
+	}
+
+	/*var refresh = function() {
+	$http.get("/studentlist").success(function(response) {
+		console.log("Response received");
+
+		$scope.studentlist = response;
+	})	
+	}
+	
+	refresh(); */
+
+	$scope.saveStudent = function() {
+		console.log($scope.stu.subject1);
+
+		/*var newString = $scope.stu.subject1;
+		alert(newString);*/
+		$http.post("/studentlist",$scope.stu).success(function(response) {
+			console.log("Data inserted client");
+			console.log(response);
+			//refresh();
+			$scope.stu="";
+		})
+	}
+
+	$scope.logout = function () {
+        $http.get('/logout')
+            .success(function (resp) {
+                if(resp === "successful"){
+                    $location.path('/login');
+                }
+            })
+    };
+
+    $scope.load_subject = function() {
+    	var selectedclass = $scope.stu.selectedClass;
+		console.log(selectedclass);
+		$http.get("/subjectlist/"+selectedclass).success(function(response) {
+			//console.log(response);
+
+			//$scope.subjects = response;
+			for(var i in response){
+			$scope.subjects = response[i].subject[i];		
+		console.log($scope.subjects);
+		}
+		})
+    }
+}])
+
+
+app.controller('LoginCtrl', ['$scope','$http','$location','$rootScope', function ($scope,$http,$location,$rootScope) {
+
+	if($rootScope.logUser == undefined){
+        $scope.logoutuser = "No one";	
+    }else {
+        $scope.logoutuser = $rootScope.logUser.username;
+    }
+
+	$scope.check = function() {
+
+		$http.post("login",$scope.user).success(function(response) {
+			if(response == "successful"){
+				$location.path("/home");
+			}
+			else{
+
+				$location.path("/");
+				$scope.error = true;
+
+			}
+			
+		})
+	}
+}])
---
+++ server.js
@@ -0,0 +1,158 @@
+var express = require('express');
+var app = express();
+var mongojs = require('mongojs');
+var db = mongojs("studentlistCopy", ["studentlist"]);
+var db_reg = mongojs("studentlistCopy", ["register"]);
+var db_class = mongojs("studentlistCopy", ["class_list"]);
+var bodyparser = require('body-parser');
+var session = require('express-session'),
+    MongoDBStore = require('connect-mongodb-session')(session)
+
+var store = new MongoDBStore({
+    uri: "mongodb://localhost:27017/studentlistCopy",
+    collection: 'login_session'
+});
+
+app.use(express.static(__dirname + "/public"));
+app.use(bodyparser.json());
+app.use(
+    session({
+        secret: 'test_sess_secret_key',
+        resave: true,
+        saveUninitialized: true,
+        store: store
+    })
+);
+
+store.on('error', function(req, res) {
+    console.log("error");
+});
+
+/*app.get("/studentlist", function(req,res) {
+	console.log("request recieved");
+
+	db.studentlist.find(function(err,docs) {
+		console.log(docs);
+		res.json(docs);
+	})
+})*/
+
+app.post("/studentlist", function(req, res) {
+    console.log("Data inserted");
+
+    console.log(req.body);
+
+    db.studentlist.insert(req.body, function(err, docs) {
+        res.json(docs);
+    })
+
+})
+
+
+app.post("/register", function(req, res) {
+    console.log("Register data");
+
+    console.log(req.body);
+
+    db_reg.register.insert(req.body, function(err, docs) {
+        res.json(docs);
+    })
+})
+
+/*app.get("/register", function(req,res) {
+	
+	console.log("login data request");
+
+	db_reg.register.find(function(err,docs) {
+		res.json(docs);
+	})
+
+	
+})*/
+
+app.post("/login", function(req, res) {
+    console.log("login data request");
+
+    db_reg.register.findOne({
+        "username": req.body.username,
+        "password": req.body.password
+    }, function(err, docs) {
+        if (docs) {
+            console.log(docs);
+            req.session.loguser = docs;
+            res.send("successful");
+        } else {
+            res.send(err);
+        }
+    })
+})
+
+app.get('/logout', function(req, res) {
+    req.session.loguser = null;
+    res.send("successful");
+});
+
+app.get('/isloggedin', function(req, res) {
+    res.send(req.session.loguser ? req.session.loguser : false);
+});
+
+app.get("/studentlist/:selectedclass", function(req, res) {
+    console.log("Does it come here");
+    var sel_class = req.params.selectedclass;
+
+    console.log(sel_class);
+    console.log("login data request");
+
+    db.studentlist.find({selectedClass: sel_class}, function(err, docs) {
+        console.log(docs);
+        res.json(docs);
+    })
+})
+
+app.get("/getstudent/:selstu", function(req, res) {
+    console.log("Does it come here again");
+    var sel_stu = req.params.selstu;
+
+    console.log(sel_stu);
+    console.log("student data request");
+
+    db.studentlist.find({name: sel_stu}, function(err, docs) {
+        console.log(docs);
+        res.json(docs);
+    })
+})
+
+/*app.post("/studentlist", function(req,res) {
+	console.log("Does it come here again");
+	
+
+	console.log(sel_stu);
+	console.log("student data request");
+
+	db.studentlist.find({"name":req.body.name}, function(err,docs) {
+		console.log(docs);
+		res.json(docs);
+	})
+})*/
+
+app.get("/class", function(req, res) {
+
+    db_class.class_list.find(function(err, docs) {
+        res.json(docs);
+    })
+})
+
+app.get("/subjectlist/:selectedclass", function(req, res) {
+
+    var selclass = req.params.selectedclass;
+    console.log(selclass);
+    db_class.class_list.find({
+        "class": selclass
+    }, function(err, docs) {
+        console.log(docs);
+        res.json(docs);
+    })
+})
+
+app.listen(3000);
+console.log("Server running on port 3000");
